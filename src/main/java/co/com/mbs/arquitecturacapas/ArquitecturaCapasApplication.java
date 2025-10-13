79e7e490d8c96f44cc52a412e0e5a32c360514ee
Camilo Gil <camilo.gil2707@hotmail.com>
2025-10-12 22:44:52 -0500

    Se realiza primer version de la arquitectura en capas

---
+++ src/main/java/co/com/mbs/arquitecturacapas/ArquitecturaCapasApplication.java
@@ -0,0 +1,13 @@
+package co.com.mbs.arquitecturacapas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturaCapasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArquitecturaCapasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/mbs/arquitecturacapas/ArquitecturaCapasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.mbs.arquitecturacapas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturaCapasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
